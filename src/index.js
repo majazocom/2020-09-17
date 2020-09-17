ec1f345706c55408ae5aa1dc181d923380deba2f
Maja Gedda <maja.gedda@zocom.se>
2020-09-17 15:42:57 +0200

    menu transitions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 function dropdown() {
+    
     let nav = document.getElementById("navigation");
+    nav.classList.toggle("show-us-the-menu");
 
-    if (nav.className === "nav") { //om klassnamnet bara är nav
-        nav.className += " show-us-the-menu";
-    } else { //om klassnamnet inte är enbart nav
-        nav.className = "nav";
-    }
+    let burger = document.getElementById("burger"); 
+    burger.classList.toggle("cross");
+
+    // let nav = document.getElementById("navigation");
+    // if (nav.className === "nav") { //om klassnamnet bara är nav
+    //     nav.className += " show-us-the-menu";
+    // } else { //om klassnamnet inte är enbart nav
+    //     nav.className = "nav";
+    // }
+
+    // let burger = document.getElementById("burger"); 
+    // if (burger.className === "burger") { //om klassnamnet bara är nav
+    //     burger.className += " cross";
+    // } else { //om klassnamnet inte är enbart nav
+    //     burger.className = "burger";
+    // }
 }
